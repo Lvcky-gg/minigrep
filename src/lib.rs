c5919b092bc798c9152cb41a5147e6d50e1c8ea0
lvcky <mail@johnodonnell.xyz>
2026-08-23 15:01:34 -0400

    chore: minigrep

---
+++ src/lib.rs
@@ -0,0 +1,55 @@
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut res = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            res.push(line.trim());
+        }
+    }
+    res
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut res = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            res.push(line.trim());
+        }
+    }
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+        Rust:
+        Safe, fast, productive.
+        Pick three,
+        Duct Tape.
+        ";
+
+        assert_eq!(vec!["Safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUst";
+        let contents = "\
+        Rust:
+        Safe, fast, productive.
+        Pick three,
+        Trust me.
+        ";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
